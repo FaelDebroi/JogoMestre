1162dc1c53453a645fc16dd28d0a96247d843b1b
Fael Debroi <rafaeldebroi@gmail.com>
2021-08-13 16:32:36 -0300

    jogo Mestre

---
+++ Mestre/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Mestre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picAmarelo = new System.Windows.Forms.PictureBox();
+            this.picAzul = new System.Windows.Forms.PictureBox();
+            this.picVerde = new System.Windows.Forms.PictureBox();
+            this.picVermelho = new System.Windows.Forms.PictureBox();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.tmrPassarCores = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdFacil = new System.Windows.Forms.RadioButton();
+            this.rdDificil = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.picAmarelo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAzul)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVerde)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVermelho)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picAmarelo
+            // 
+            this.picAmarelo.Enabled = false;
+            this.picAmarelo.Image = global::Mestre.Properties.Resources.amarelo;
+            this.picAmarelo.Location = new System.Drawing.Point(73, 33);
+            this.picAmarelo.Name = "picAmarelo";
+            this.picAmarelo.Size = new System.Drawing.Size(261, 224);
+            this.picAmarelo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picAmarelo.TabIndex = 0;
+            this.picAmarelo.TabStop = false;
+            this.picAmarelo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picAmarelo_MouseDown);
+            this.picAmarelo.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picAmarelo_MouseUp);
+            // 
+            // picAzul
+            // 
+            this.picAzul.Enabled = false;
+            this.picAzul.Image = global::Mestre.Properties.Resources.azul;
+            this.picAzul.Location = new System.Drawing.Point(330, 33);
+            this.picAzul.Name = "picAzul";
+            this.picAzul.Size = new System.Drawing.Size(261, 224);
+            this.picAzul.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picAzul.TabIndex = 1;
+            this.picAzul.TabStop = false;
+            this.picAzul.Click += new System.EventHandler(this.picAzul_Click);
+            this.picAzul.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picAzul_MouseDown);
+            this.picAzul.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picAzul_MouseUp);
+            // 
+            // picVerde
+            // 
+            this.picVerde.Enabled = false;
+            this.picVerde.Image = global::Mestre.Properties.Resources.verde;
+            this.picVerde.Location = new System.Drawing.Point(73, 257);
+            this.picVerde.Name = "picVerde";
+            this.picVerde.Size = new System.Drawing.Size(261, 224);
+            this.picVerde.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picVerde.TabIndex = 2;
+            this.picVerde.TabStop = false;
+            this.picVerde.Click += new System.EventHandler(this.picVerde_Click);
+            this.picVerde.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picVerde_MouseDown);
+            this.picVerde.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picVerde_MouseUp);
+            // 
+            // picVermelho
+            // 
+            this.picVermelho.Enabled = false;
+            this.picVermelho.Image = global::Mestre.Properties.Resources.vermelho;
+            this.picVermelho.Location = new System.Drawing.Point(330, 257);
+            this.picVermelho.Name = "picVermelho";
+            this.picVermelho.Size = new System.Drawing.Size(261, 224);
+            this.picVermelho.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picVermelho.TabIndex = 3;
+            this.picVermelho.TabStop = false;
+            this.picVermelho.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picVermelho_MouseDown);
+            this.picVermelho.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picVermelho_MouseUp);
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicio.Location = new System.Drawing.Point(249, 231);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(169, 60);
+            this.btnInicio.TabIndex = 4;
+            this.btnInicio.Text = "Começar!";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // tmrPassarCores
+            // 
+            this.tmrPassarCores.Interval = 500;
+            this.tmrPassarCores.Tick += new System.EventHandler(this.tmrPassarCores_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdDificil);
+            this.groupBox1.Controls.Add(this.rdFacil);
+            this.groupBox1.Location = new System.Drawing.Point(597, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(145, 138);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dificuldade";
+            // 
+            // rdFacil
+            // 
+            this.rdFacil.AutoSize = true;
+            this.rdFacil.Checked = true;
+            this.rdFacil.Location = new System.Drawing.Point(24, 46);
+            this.rdFacil.Name = "rdFacil";
+            this.rdFacil.Size = new System.Drawing.Size(54, 21);
+            this.rdFacil.TabIndex = 0;
+            this.rdFacil.TabStop = true;
+            this.rdFacil.Text = "facil";
+            this.rdFacil.UseVisualStyleBackColor = true;
+            // 
+            // rdDificil
+            // 
+            this.rdDificil.AutoSize = true;
+            this.rdDificil.Location = new System.Drawing.Point(24, 91);
+            this.rdDificil.Name = "rdDificil";
+            this.rdDificil.Size = new System.Drawing.Size(60, 21);
+            this.rdDificil.TabIndex = 1;
+            this.rdDificil.Text = "dificil";
+            this.rdDificil.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 520);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnInicio);
+            this.Controls.Add(this.picVermelho);
+            this.Controls.Add(this.picVerde);
+            this.Controls.Add(this.picAzul);
+            this.Controls.Add(this.picAmarelo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picAmarelo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAzul)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVerde)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVermelho)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picAmarelo;
+        private System.Windows.Forms.PictureBox picAzul;
+        private System.Windows.Forms.PictureBox picVerde;
+        private System.Windows.Forms.PictureBox picVermelho;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Timer tmrPassarCores;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdDificil;
+        private System.Windows.Forms.RadioButton rdFacil;
+    }
+}
+
---
+++ Mestre/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mestre
+{
+    public partial class Form1 : Form
+    {
+
+        int[] cores;
+        int atual = 0;
+        int ultima = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void picAzul_Click(object sender, EventArgs e)
+        {
+            
+
+            
+        }
+
+        private void picAzul_MouseDown(object sender, MouseEventArgs e)
+        {
+            picAzul.Image = Properties.Resources.azul_aceso;
+        }
+
+        private void picAzul_MouseUp(object sender, MouseEventArgs e)
+        {
+            picAzul.Image = Properties.Resources.azul;
+            novaResposta(2);
+        }
+
+        private void picAmarelo_MouseDown(object sender, MouseEventArgs e)
+        {
+            picAmarelo.Image = Properties.Resources.amarelo_aceso;
+        }
+
+        private void picAmarelo_MouseUp(object sender, MouseEventArgs e)
+        {
+            picAmarelo.Image = Properties.Resources.amarelo;
+            novaResposta(1);
+        }
+
+        private void picVerde_MouseDown(object sender, MouseEventArgs e)
+        {
+            picVerde.Image = Properties.Resources.verde_aceso;
+        }
+
+        private void picVerde_MouseUp(object sender, MouseEventArgs e)
+        {
+            picVerde.Image = Properties.Resources.verde;
+            novaResposta(3);
+        }
+
+        private void picVermelho_MouseDown(object sender, MouseEventArgs e)
+        {
+            picVermelho.Image = Properties.Resources.vermelho_aceso;
+        }
+
+        private void picVermelho_MouseUp(object sender, MouseEventArgs e)
+        {
+            picVermelho.Image = Properties.Resources.vermelho;
+            novaResposta(4);
+        }
+
+        private void finalizarJogo()
+        {
+            habilitarBotoes(false);
+            btnInicio.Enabled = true;
+            ultima = 0;
+        }
+
+        private void habilitarBotoes(bool estado)
+        {
+            picAmarelo.Enabled = estado;
+            picAzul.Enabled = estado;
+            picVerde.Enabled = estado;
+            picVermelho.Enabled = estado;
+        }
+
+        private void novaResposta(int corClicada)
+        {
+            //se ele errou, terminar o jogo
+            if(corClicada != cores[atual])
+            {
+                MessageBox.Show("Você PERDEU! Tente novamente do inicio!");
+                //terminar o jogo
+                finalizarJogo();
+            }
+            else //acertei a cor
+            {
+                if(atual < ultima) //acertei parcialmente
+                {
+                    atual++;
+                }
+                else //acertei tudo
+                {
+                    ultima++;
+                    if (ultima >= cores.Length)//acertei até a ultima cor do tamanho do array
+                    {
+                        MessageBox.Show("VOCÊ GANHOU!! UHUUUL!");
+                        finalizarJogo();
+                    }
+                    else
+                    {
+                        habilitarBotoes(false);
+                        piscarNova();
+                    }
+                }
+            }
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            btnInicio.Enabled = false;
+
+            if (rdFacil.Checked)
+                cores = new int[4];
+            else
+                cores = new int[8];
+
+            iniciarJogo();
+        }
+
+        private void iniciarJogo()
+        {
+            //sortear as cores
+            Random rand = new Random();
+
+            //colocar a primeira cor
+            cores[0] = rand.Next(1, 5);
+            //gerando as outras cores da sequencia!
+            for (int pos = 1; pos < cores.Length; pos++)
+            {
+                int temp = rand.Next(1, 5); //{1- amar. | 2 - azul | 3 - verde | 4 - verm.} 
+                //se for igual, não uso!
+                //Se for diferente da anterior, eu uso
+                if(temp != cores[pos-1])
+                {
+                    cores[pos] = temp;
+                }
+                else
+                {
+                    pos--;
+                }
+            }
+            //piscar as cores
+            piscarNova();
+        }
+
+        private void piscarNova()
+        {
+            atual = 0;
+            tmrPassarCores.Enabled = true;
+        }
+
+        private void tmrPassarCores_Tick(object sender, EventArgs e)
+        {
+            //apaga a cor
+            picAmarelo.Image = Properties.Resources.amarelo;
+            picAzul.Image = Properties.Resources.azul;
+            picVerde.Image = Properties.Resources.verde;
+            picVermelho.Image = Properties.Resources.vermelho;
+
+            //acende a próxima cor
+            if (atual <= ultima)
+            {
+                switch (cores[atual])
+                {
+                    case 1:
+                        picAmarelo.Image = Properties.Resources.amarelo_aceso;
+                        break;
+                    case 2:
+                        picAzul.Image = Properties.Resources.azul_aceso;
+                        break;
+                    case 3:
+                        picVerde.Image = Properties.Resources.verde_aceso;
+                        break;
+                    case 4:
+                        picVermelho.Image = Properties.Resources.vermelho_aceso;
+                        break;
+                }
+                atual++;
+            }
+            else //vez do usuário repetir
+            {
+                atual = 0;
+                tmrPassarCores.Enabled = false;
+                habilitarBotoes(true);
+            }
+        }
+
+        private void picVerde_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
